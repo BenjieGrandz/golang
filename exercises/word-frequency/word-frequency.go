1b6aa29acab067572f08118411dc7f73f0641b7e
bkoimett <koimettb@gmail.com>
2025-05-08 16:50:06 +0300

    updating word-frequency.go using map

--- exercises/word-frequency/word-frequency.go
+++ exercises/word-frequency/word-frequency.go
@@ -1,13 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func wordFrequency(s string) {
-	word := make(map[string]int)
+func wordFrequency(s string) map[string]int {
+	// empty map i.e wordCOunts := {}
+	wordCounts := make(map[string]int)
 
-	for _, char := 
+	// splits string to slice i.e words = ["go", "go"....]
+	words := strings.Fields(s)
+
+	// loop through each word and update map
+	for _, word := range words {
+		wordCounts[word]++
+	}
+
+	// output the map
+	for word, count := range wordCounts{	// word is the key and count is the value
+		fmt.Printf("%s: %d\n", word, count)
+	}
+
+	return wordCounts
 }
 
 func main() {
 	s := "go go gophers are awesome go"
+	fmt.Println(wordFrequency(s))
 }
+
+// map iteration is randomized thus the printf func
