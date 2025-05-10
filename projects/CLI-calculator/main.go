c48d5652de7788d32381923570136d5efea304fe
bkoimett <koimettb@gmail.com>
2025-05-10 13:32:33 +0300

    uploading first project

---
+++ projects/CLI-calculator/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Println("Please enter : [add/sub/div/multi] [num1] [num2]")
+		return
+	}
+	
+	op := os.Args[1]
+	num1 := os.Args[2]
+	num2 := os.Args[3]
+
+	a, err1 := strconv.ParseFloat(num1, 64)
+	b, err2 := strconv.ParseFloat(num2, 64)
+
+	if err1 != nil && err2 != nil {
+		fmt.Println("Error : both arguments must be integers")
+		return
+	}
+
+	switch op {
+	case "add" :
+		fmt.Println("Result: ", a+b)
+	case "sub":
+		fmt.Println("Result: ", a-b)
+	case "div":
+		fmt.Println("Result: ", a/b)
+	case "multi":
+		fmt.Println("Result: ", a*b)
+	default :
+		fmt.Println("Unknown Operation [add/div/multi/sub]")
+	}
+	
+}
