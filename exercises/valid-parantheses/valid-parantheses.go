bfc46c7433dd9c59c5d9dad474b1c7d399945bb0
bkoimett <koimettb@gmail.com>
2025-05-11 08:56:03 +0300

    update parantheses

---
+++ exercises/valid-parantheses/valid-parantheses.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func validParantheses(s string) bool {
+	stack := []int{}
+
+}
+
+func main() {
+    testCases := []string{
+        "()",        // true
+        "()[]{}",    // true
+        "(]",        // false
+        "([)]",      // false
+        "{[]}",      // true
+        "",          // true
+        "{[}",       // false
+        "([]{})",    // true
+        "((((",      // false
+        "(()())",    // true
+    }
+
+    for _, test := range testCases {
+        fmt.Printf("Input: %q -> Valid: %v\n", test, isValid(test))
+    }
+}
