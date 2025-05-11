7fae77f0573fe9ea0e97e2f279e3a51e615dc7a3
bkoimett <koimettb@gmail.com>
2025-05-11 09:12:35 +0300

    used switch case to push and pop stack

--- exercises/valid-parantheses/valid-parantheses.go
+++ exercises/valid-parantheses/valid-parantheses.go
@@ -3,13 +3,25 @@ package main
 import "fmt"
 
 func isValid(s string) bool {
-	stack := []int{}
+	stack := []rune{}
 	bracketMap := map[rune]rune {
 		')' : '(',
 		']' : '[',
 		'}' : '{',
 	}
 
+	for _, char := range s {
+		switch char {
+		case '(', '[', '{' :
+			stack = append(stack, char)  // push
+		case ')', ']', '}' :
+			if len(stack) <= 0 || stack[len(stack)-1] != bracketMap[char] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		}
+	}
+	
 }
 
 func main() {
