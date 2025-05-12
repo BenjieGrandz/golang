71f1864e2b9cac4e04887f2f6ebae717723959cd
bkoimett <koimettb@gmail.com>
2025-05-12 21:15:49 +0300

    file update

---
+++ exercises/file-I/O/file-I/o.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"io"
+)
+
+// file, err := os.Create("file.txt")
+// defer file.Close()
+
+// file.WriteString("Hello\n")
+
+// file, _ := os.Open("file.txt")
+// scanner := bufio.NewScanner(file)
+// for scanner.Scan() {
+//     fmt.Println(scanner.Text())
+// }
