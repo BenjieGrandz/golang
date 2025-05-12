575f47459ebe22c94f5e1c217ca22799de4efe19
bkoimett <koimettb@gmail.com>
2025-05-12 21:02:42 +0300

    uploading error-handling.go

--- exercises/error-handling/error-handling.go
+++ exercises/error-handling/error-handling.go
@@ -7,11 +7,15 @@ import (
 
 // Divide returns the result of dividing a by b, or an error if b is zero.
 func Divide(a, b float64) (float64, error) {
-    
+    if b == 0 {
+        return 0, errors.New("cannot divide by zero")
+    }
+
+    return a/b, nil
 }
 
 func main() {
-    a, b := 10.0, 0.0
+    a, b := 10.0, 2.0
     result, err := Divide(a, b)
     if err != nil {
         fmt.Println("Error:", err)
