e887ff8013be3295b90132836d364bfcf991efa7
bkoimett <koimettb@gmail.com>
2025-05-12 20:50:37 +0300

    update word Frequency

--- exercises/word-frequency/word-frequency.go
+++ exercises/word-frequency/word-frequency.go
@@ -6,23 +6,18 @@ import (
 )
 
 func wordFrequency(s string) map[string]int {
-	// empty map i.e wordCOunts := {}
-	wordCounts := make(map[string]int)
-
-	// splits string to slice i.e words = ["go", "go"....]
+	wordCount := make(map[string]int)
 	words := strings.Fields(s)
 
-	// loop through each word and update map
 	for _, word := range words {
-		wordCounts[word]++
+		wordCount[word]++
 	}
 
-	// output the map
-	for word, count := range wordCounts {	// word is the key and count is the value
-		fmt.Printf("%s: %d\n", word, count)
+	for word, count := range wordCount {
+		fmt.Println(word, count)
 	}
 
-	return wordCounts
+	return wordCount
 }
 
 func main() {
