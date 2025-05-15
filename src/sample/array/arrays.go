cd9ff65d76ff2bafc723c033329539c282b99b85
bkoimett <koimettb@gmail.com>
2025-05-15 13:36:42 +0300

    commeting on arrays

--- src/sample/array/arrays.go
+++ src/sample/array/arrays.go
@@ -3,25 +3,29 @@ package main
 import "fmt"
 
 func main() {
-
-    var a [5]int
+	// empty array/default -- holds 5 int 
+    var a [6]int
     fmt.Println("emp:", a)
 
-    a[4] = 100
+    a[4] = 100 //seting an index specifically
     fmt.Println("set:", a)
     fmt.Println("get:", a[4])
 
     fmt.Println("len:", len(a))
 
+	// declare and initialize
     b := [5]int{1, 2, 3, 4, 5}
     fmt.Println("dcl:", b)
 
+	// automatically counts elements
     b = [...]int{1, 2, 3, 4, 5}
     fmt.Println("dcl:", b)
 
-    b = [...]int{100, 3: 400, 500}
+	// anything in between will be zeroed
+    b = [...]int{100, 3: 700, 500}
     fmt.Println("idx:", b)
 
+	// 2d arrays
     var twoD [2][3]int
     for i := 0; i < 2; i++ {
         for j := 0; j < 3; j++ {
@@ -30,6 +34,7 @@ func main() {
     }
     fmt.Println("2d: ", twoD)
 
+	// create and declare multiarreys
     twoD = [2][3]int{
         {1, 2, 3},
         {1, 2, 3},
