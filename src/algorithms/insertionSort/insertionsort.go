56dcffd06c028ef7b9a53dfc6445b9f80284bf59
bkoimett <koimettb@gmail.com>
2025-05-17 16:46:27 +0300

    updated inset sort

--- src/algorithms/insertionSort/insertionsort.go
+++ src/algorithms/insertionSort/insertionsort.go
@@ -6,3 +6,19 @@ func main() {
 	x := []int{7, 6, 3, 9, 1, 2}
 	fmt.Println(insertSort(x))
 }
+
+func insertSort(n []int) []int {
+	for i := 0; i < len(n)-1; i++ {
+		key := n[i]
+		j := i - 1
+
+		for j >= 0 && n[j+1] > key {
+			n[j+1] = n[j]
+			j--
+		}
+
+		n[j+1] = key
+	}
+
+	return n
+}
