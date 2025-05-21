7f9a91c784a964bfdc5614ee8aba74f35fe5e6de
bkoimett <benkoimet@gmail.com>
2025-05-21 13:17:51 +0300

    feat(linearS): add search function

--- src/algorithms/linearsearch/linearsearch.go
+++ src/algorithms/linearsearch/linearsearch.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println(linearS(4, 2))
+	x := []int{3, 4, 7, 2, 8,15}
+	fmt.Println(linearS(x, 2))
 }
 
 // implementing linear search
+func linearS(n []int, target int) int {
+	for _, v := range n {
+		if v == target {
+			return v
+		}
+	}
+
+	return -1
+}
