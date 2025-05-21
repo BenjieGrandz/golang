4f5fc9a4483bb194ad436b2c363a8dcd5831d109
bkoimett <benkoimet@gmail.com>
2025-05-21 17:34:28 +0300

    refactor(main): format file

--- src/algorithms/binarysearch/binarysearch.go
+++ src/algorithms/binarysearch/binarysearch.go
@@ -10,7 +10,7 @@ func main() {
 
 func binaryS(arr []int, target int) int {
 	low := 0
-	high := len(arr)-1
+	high := len(arr) - 1
 
 	for low <= high {
 		mid := low + (high-low)/2
@@ -20,7 +20,7 @@ func binaryS(arr []int, target int) int {
 		} else if arr[mid] < target {
 			low = mid + 1
 		} else {
-			high = mid -1
+			high = mid - 1
 		}
 	}
 
