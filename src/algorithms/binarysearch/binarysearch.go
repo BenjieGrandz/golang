0db93210475c6f598da8528339f69d8cf77ca134
bkoimett <benkoimet@gmail.com>
2025-05-21 17:32:51 +0300

    feat(binaryS): add new binarySearch function

--- src/algorithms/binarysearch/binarysearch.go
+++ src/algorithms/binarysearch/binarysearch.go
@@ -5,5 +5,24 @@ import "fmt"
 func main() {
 	x := []int{1, 3, 5, 7, 9, 11, 13}
 	y := 9
-	fmt.Printf("The target : %v is at index: %v", y, binaryS(x))
+	fmt.Printf("The target : %v is at index: %v", y, binaryS(x, y))
+}
+
+func binaryS(arr []int, target int) int {
+	low := 0
+	high := len(arr)-1
+
+	for low <= high {
+		mid := low + (high-low)/2
+
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid -1
+		}
+	}
+
+	return -1
 }
