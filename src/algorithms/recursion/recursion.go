5ae045bbaa178c5cb4a1c2dad05f692c3a25b093
bkoimett <benkoimet@gmail.com>
2025-05-21 13:59:26 +0300

    chore: create boilerplate

---
+++ src/algorithms/recursion/recursion.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func(main) {
+	num1 := 7
+	num2 := 9
+	fmt.Printf("For %s the Factorial is: %s while the Fibonacci is: %s \n",num1 fact(num1), fib(num1))
+	fmt.Printf("For %s the Factorial is: %s while the Fibonacci is: %s \n",num2, fact(num2), fib(num2))
+}
