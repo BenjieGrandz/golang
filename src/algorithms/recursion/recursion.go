509bb27582df0766419fcedfdefb451d959810f5
bkoimett <benkoimet@gmail.com>
2025-05-21 14:00:20 +0300

    fix(main): add comma toseparate variables

--- src/algorithms/recursion/recursion.go
+++ src/algorithms/recursion/recursion.go
@@ -5,6 +5,6 @@ import "fmt"
 func(main) {
 	num1 := 7
 	num2 := 9
-	fmt.Printf("For %s the Factorial is: %s while the Fibonacci is: %s \n",num1 fact(num1), fib(num1))
+	fmt.Printf("For %s the Factorial is: %s while the Fibonacci is: %s \n",num1 ,fact(num1), fib(num1))
 	fmt.Printf("For %s the Factorial is: %s while the Fibonacci is: %s \n",num2, fact(num2), fib(num2))
 }
