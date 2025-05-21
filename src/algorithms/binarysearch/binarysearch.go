577f4d3344713c2f790409753651f988f685e3b1
bkoimett <benkoimet@gmail.com>
2025-05-21 17:27:39 +0300

    fix(main): fix x variable declaration

--- src/algorithms/binarysearch/binarysearch.go
+++ src/algorithms/binarysearch/binarysearch.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	x := {1, 3, 5, 7, 9, 11, 13}
+	x := []int{1, 3, 5, 7, 9, 11, 13}
 	y := 9
 	fmt.Printf("The target : %v is at index: %v", y, binaryS(x))
 }
