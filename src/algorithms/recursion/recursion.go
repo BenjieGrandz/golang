9321847c225f530de6ece4f1685912603a582190
bkoimett <benkoimet@gmail.com>
2025-05-21 14:07:50 +0300

    feat(fib/fact): add new functions factorial and fibonacci using recursion

--- src/algorithms/recursion/recursion.go
+++ src/algorithms/recursion/recursion.go
@@ -2,9 +2,25 @@ package main
 
 import "fmt"
 
-func(main) {
+func main() {
 	num1 := 7
 	num2 := 9
-	fmt.Printf("For %s the Factorial is: %s while the Fibonacci is: %s \n",num1 ,fact(num1), fib(num1))
-	fmt.Printf("For %s the Factorial is: %s while the Fibonacci is: %s \n",num2, fact(num2), fib(num2))
+	fmt.Printf("For %v the Factorial is: %v while the Fibonacci is: %v \n",num1 ,fact(num1), fib(num1))
+	fmt.Printf("For %v the Factorial is: %v while the Fibonacci is: %v \n",num2, fact(num2), fib(num2))
+}
+
+func fact(n int) int {
+	if n == 0 {
+		return -1
+	}
+
+	return n * fact(n-1)
+}
+
+func fib(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	return fib(n-1) + fib(n-2)
 }
