11540a3f8c670027a2cecde1016c5621e05614c3
bkoimett <benkoimet@gmail.com>
2025-05-22 15:27:47 +0300

    feat: added the sort function

--- code/sortStruct/sortStruct.go
+++ code/sortStruct/sortStruct.go
@@ -8,6 +8,14 @@ type Person struct {
     Age  int
 }
 
+func linearSort(n []int) []int {
+	for i:=0; i<len(n); i++ {
+		key:n[i]
+		j := i-1
+	}
+
+}
+
 func main(){
 	customerBase := []Person{
 		{Name: "Benjie", Age: 30},
