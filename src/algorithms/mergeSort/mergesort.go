3b5298cb39110784e811cebc15883ff966df2e93
bkoimett <benkoimet@gmail.com>
2025-05-23 20:02:43 +0500

    feat: add mergesort function

--- src/algorithms/mergeSort/mergesort.go
+++ src/algorithms/mergeSort/mergesort.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+func mergeSort(arr []int) []int{
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	mid := len(arr)/2
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+
+	return merge(left, right)
+}
+
 func main(){
 	num := []ibt{2,8,3,7,4,6,5}
 	fmt.Println(mergeSort(num))
