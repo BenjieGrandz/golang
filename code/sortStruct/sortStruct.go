e188727e2b3056910857890dba98d4fa5c6fa129
bkoimett <benkoimet@gmail.com>
2025-05-25 00:08:30 +0300

    style: formate entire document

--- code/sortStruct/sortStruct.go
+++ code/sortStruct/sortStruct.go
@@ -4,14 +4,14 @@ package main
 import "fmt"
 
 type Person struct {
-    Name string
-    Age  int
+	Name string
+	Age  int
 }
 
 func insertionSort(n []Person) []Person {
-	for i:=0; i<len(n); i++ {
-		key:= n[i].Age
-		j := i-1
+	for i := 0; i < len(n); i++ {
+		key := n[i].Age
+		j := i - 1
 
 		for j >= 0 && n[j].Age > key {
 			n[j+1].Age = n[j].Age
@@ -25,7 +25,7 @@ func insertionSort(n []Person) []Person {
 
 }
 
-func main(){
+func main() {
 	customerBase := []Person{
 		{Name: "Benjie", Age: 30},
 		{Name: "Nyaigoti", Age: 23},
