2669237d51cc0ecf8f5c2bd153cd935ea0883b5e
bkoimett <benkoimet@gmail.com>
2025-05-24 09:02:54 +0300

    style : format entire doc

--- projects/week1finalproject/main.go
+++ projects/week1finalproject/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
 	"strings"
 )
 
@@ -11,10 +11,10 @@ import (
 func bubbleSort(arr []int) []int {
 	n := len(array)
 
-	for i:=0; i<n-1; i++ {
+	for i := 0; i < n-1; i++ {
 		swapped := false
-		
-		for j:=0; j<n-i-1; j++ {
+
+		for j := 0; j < n-i-1; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
 				swapped = true
