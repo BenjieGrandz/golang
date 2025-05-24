f1533c152a6361fb0c80b97c6748b1bf8c5bfee8
bkoimett <benkoimet@gmail.com>
2025-05-24 09:08:32 +0300

    feat: add insetion sort algorithm

--- projects/week1finalproject/main.go
+++ projects/week1finalproject/main.go
@@ -29,6 +29,21 @@ func bubbleSort(arr []int) []int {
 	return arr
 }
 
+// insertion sort
+func insertionSort(arr []int) []int {
+	for i:=1; i<len(arr); i++ {
+		key := arr[i]
+		j := i-1
+
+		for j > 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+		}
+		arr[j+1] = key
+	}
+	
+	return arr
+}
+
 func main() {
 	nums := []int{}
 
