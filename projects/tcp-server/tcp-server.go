5e02024e04b79acd70ec46810f2f56e8a0ee81e0
bkoimett <benkoimet@gmail.com>
2025-05-25 19:34:09 +0300

    feat (tcp-server): create  listener object

--- projects/tcp-server/tcp-server.go
+++ projects/tcp-server/tcp-server.go
@@ -1,10 +1,19 @@
 package main
 
 inmport (
+	"bfio"
 	"fmt"
 	"net"
 )
 
 func msin() {
-	
+	ln, err := net.listen('tcp', '8080')
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	defer ln.close()
+
+	fmt.Println("Server is running on port 8080...")
 }
