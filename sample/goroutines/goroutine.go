04996dde01c75822b9167c063b3324e78c403b8f
bkoimett <benkoimet@gmail.com>
2025-05-25 10:06:07 +0300

    style: format gouroutine.go

--- sample/goroutines/goroutine.go
+++ sample/goroutines/goroutine.go
@@ -1,12 +1,12 @@
 package main
 
 import (
- 	"fmt"
- 	"time"
+	"fmt"
+	"time"
 )
 
 func say(s string) {
-	for i:=0; i<5;i++{
+	for i := 0; i < 5; i++ {
 		time.Sleep(1000 * time.Millisecond)
 		fmt.Println(s)
 	}
