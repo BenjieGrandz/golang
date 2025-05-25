c4f9215494e167ee44c495d8e829a354c96fa27f
bkoimett <benkoimet@gmail.com>
2025-05-25 21:16:08 +0300

    chore: create boiler plate for tcp client

---
+++ projects/tcp-client/tcp-client.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"log"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		log.Println("Connection error: ", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to server. Type messages and press < ENTER > .")
+
+	for {
+		fmt.Print("> ")
+		text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		if text == "exit\n" {
+			fmt.Println("Closing connection.")
+			return
+		}
+
+		conn.Write([]byte(text))
+
+		reply, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Println("Server replied: ", reply)
+	}
+}
