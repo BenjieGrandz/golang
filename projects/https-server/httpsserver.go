8d5f7041ddda48fd3dc7d53af380fc601c063750
bkoimett <benkoimet@gmail.com>
2025-05-28 10:19:30 +0300

    feat (https) : add packages to be used for project

--- projects/https-server/httpsserver.go
+++ projects/https-server/httpsserver.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"crypto/rand" // random secure number generator
+	"crypto/rsa"  // rsa key generation
+	"crypto/tls"	// TLS support in go
+	"crypto/x509"	// certificate creation and parsing
+	"crypto/x509/pkix" // certificate name fields (subject, issuer, etc.)
+	"encoding/pem"	// PEM encoding (text format for keys/certs)
 	"fmt"
+	"math/big"	// generates serial numbers
+	"net/http"
+	"time"
 )
 
 func main() {
-	
+
 }
