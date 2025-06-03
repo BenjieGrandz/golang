e06f0d964cabcf738ed617a8829c853680e98e28
bkoimett <benkoimet@gmail.com>
2025-06-03 13:22:48 +0300

    style (based-numbers) : format file

--- projects/based-numbers/based-numbers.go
+++ projects/based-numbers/based-numbers.go
@@ -10,8 +10,8 @@ func main() {
 	fmt.Scan(&myNumber)
 
 	binary := ""
-	for i:=myNumber; i>=1; i=i/2 {
-		if i % 2 == 0 {
+	for i := myNumber; i >= 1; i = i / 2 {
+		if i%2 == 0 {
 			binary = "0" + binary
 		} else {
 			binary = "1" + binary
