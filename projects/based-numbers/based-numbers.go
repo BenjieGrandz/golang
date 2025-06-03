67f9cb32e18c0ae8257379dbcf989e8e5259225a
bkoimett <benkoimet@gmail.com>
2025-06-03 13:20:28 +0300

    Initial commit

---
+++ projects/based-numbers/based-numbers.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var myNumber int
+	fmt.Println("Enter number to be converted: ")
+	fmt.Scan(&myNumber)
+
+	binary := ""
+	for i:=myNumber; i>=1; i=i/2 {
+		if i % 2 == 0 {
+			binary = "0" + binary
+		} else {
+			binary = "1" + binary
+		}
+	}
+
+	fmt.Printf("Then binary of %v is: %v\n", myNumber, binary)
+}
