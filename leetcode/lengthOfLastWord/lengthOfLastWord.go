dc8e611ddd8fcdd02677650c2c527275539d4384
bkoimett <benkoimet@gmail.com>
2025-06-10 09:06:49 +0300

    styrle : format lengthOfLastWord

--- leetcode/lengthOfLastWord/lengthOfLastWord.go
+++ leetcode/lengthOfLastWord/lengthOfLastWord.go
@@ -5,18 +5,18 @@ import (
 	"strings"
 )
 
-func lengthOfLastWord(s string) int{
+func lengthOfLastWord(s string) int {
 	words := strings.Fields(s)
 
-	for index, word := range words{
-		if index == len(words)-1{
+	for index, word := range words {
+		if index == len(words)-1 {
 			return len(word)
 		}
 	}
 	return -1
 }
 
-func main(){
+func main() {
 	str := "  fly me	to the moon		"
 	fmt.Println(lengthOfLastWord(str))
 }
